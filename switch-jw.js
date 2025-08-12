42f67c84d5ecc72372dd5f68120c2fd0fc8190b4
F9v7K <ian.17bussines@gmail.com>
2025-08-12 09:49:19 +0700

    initila commit

---
+++ switch-jw.js
@@ -0,0 +1,43 @@
+// PLAYER :
+var rematch = true;
+
+while (rematch == true) {
+    var player = prompt('Apa Pilihan Kamu: \n(cth gajah, semut, orang)');
+    // VS COMPUTER : Math.random() adalah fungsi untuk menggenerate angka random/ random value.
+    var computer = Math.random();
+
+    // deklarasi:
+    if (computer < 0.35) {
+        computer = 'semut';
+    } else if (computer >= 0.35 && computer < 0.68) {
+        computer = 'orang';
+    } else {
+        computer = 'gajah';
+    }
+
+    // RULES :
+    var hasil = '';
+
+    if (player == computer) {
+        hasil = 'SERI';
+    } else if (player == 'orang') {
+        // if (computer == 'semut') {
+        //     hasil == 'MENANG';
+        // } else {
+        //     hasil = 'KALAH!';
+        // }
+        // operator ternary:
+        hasil = (computer == 'gajah') ? 'KALAH!' : 'MENANG';
+    } else if (player == 'orang') {
+        hasil = (computer = 'semut') ? 'MENANG' : 'KALAH!';
+    }
+    else if (player == 'semut') {
+        hasil = (computer == 'gajah') ? 'MENANG' : 'KALAH!';
+    } else {
+        hasil = 'kamu memasukan pilihan yang salah!';
+    }
+    // TAMPIL HASIL 
+    alert(`Kamu : ${player} \nvs\nComputer : ${computer} \n\nHasilnya :\nKamu ${hasil}`);
+    rematch = confirm('Suit Ulang?');
+}
+alert('Terima Kasih Telah Bermain!');
